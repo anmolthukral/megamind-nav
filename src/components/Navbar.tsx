697d3336e3781b46cde0ddcf08656191b5a39089
Anmol Thukral <thukral.anmol21@gmail.com>
2026-02-14 00:42:43 +0530

    feat: added design system

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,14 +9,8 @@ const Navbar: React.FC = () => {
       </a>
 
       <div style={{ marginLeft: 'auto', display: 'flex', gap: 'var(--space-4)' }}>
-        {/* Desktop Links - hidden on mobile via CSS media query if we strictly followed Oat, 
-            but here we'll use a simple inline style for now or standard Oat classes. 
-            Oat doesn't have a 'hidden-md' utility by default in the minified version I saw, 
-            so we might rely on standard CSS or the 'is-mobile' pattern if available.
-            For now, let's just render them as simple links as Oat intended for topnav.
-        */}
-        <a href="/tutorials" onClick={(e) => { e.preventDefault(); window.location.href = '/tutorials'; }}>Tutorials</a>
         <a href="/blogs" onClick={(e) => { e.preventDefault(); window.location.href = '/blogs'; }}>Blogs</a>
+        <a href="/tutorials" onClick={(e) => { e.preventDefault(); window.location.href = '/tutorials'; }}>Tutorials</a>
         <a href="/profile" onClick={(e) => { e.preventDefault(); window.location.href = '/profile'; }}>Profile</a>
         <a href="/tutorials/playground" onClick={(e) => { e.preventDefault(); window.location.href = '/tutorials/playground'; }}>Playground</a>
       </div>
